91b5853048a8070dbb07162c483934e3c65d7e2b
estherkoko <estherkolade@gmail.com>
2018-12-06 01:02:23 -0500

    files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'notyoutube'`, async(() => {
+  it(`should have as title 'alegna'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('notyoutube');
+    expect(app.title).toEqual('alegna');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to notyoutube!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to alegna!');
   }));
 });
