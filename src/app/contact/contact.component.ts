104c1adb3f91fc4405ad765b04853b0cdcdd4f85
adeadedoja <damdey@yahoo.com>
2018-12-21 21:19:52 +0200

    Master - Fix for form submitting for contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -9,19 +9,17 @@ import * as emailjs from 'emailjs-com';
 })
 export class ContactComponent implements OnInit {
 
- 
   constructor() { }
 
   ngOnInit() {
   }
 
- onContactSubmit(){
-   emailjs.sendForm('alegna_service', 'contact_form', 'contact-form', 'user_QmgUSDLy2M8ifZ5i28u62')
+ onContactSubmit(contactForm: any) {
+   emailjs.sendForm('alegna_service', 'contact_form', '#contact-form', 'user_QmgUSDLy2M8ifZ5i28u62')
     .then((response) => {
       console.log('SUCCESS!', response.status, response.text);
     }, (err) => {
       console.log('FAILED...', err);
     });
-    //form.reset();
  }
 }
