0aceeb1b33c3a0e629229598b0c3b0faa74e66e2
estherkoko <estherkolade@gmail.com>
2018-12-21 16:38:38 -0500

    fixed contact form functionality

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import * as emailjs from 'emailjs-com';
 
 @Component({
@@ -9,17 +9,34 @@ import * as emailjs from 'emailjs-com';
 })
 export class ContactComponent implements OnInit {
 
+  isLoading: boolean = false;
+  senderName: String = '';
+
   constructor() { }
 
   ngOnInit() {
   }
 
- onContactSubmit(contactForm: any) {
-   emailjs.sendForm('alegna_service', 'contact_form', '#contact-form', 'user_QmgUSDLy2M8ifZ5i28u62')
-    .then((response) => {
-      console.log('SUCCESS!', response.status, response.text);
-    }, (err) => {
-      console.log('FAILED...', err);
-    });
- }
+  onContactSubmit(contactForm: any) {
+    this.isLoading = true;
+    this.senderName = contactForm.value.fullname;
+
+    emailjs.sendForm('alegna_service', 'contact_form', '#contact-form', 'user_QmgUSDLy2M8ifZ5i28u62')
+      .then((response) => {
+        this.isLoading = false;
+        console.log('SUCCESS!', response.status, response.text);
+        this.hideAlertAfterSeconds(2000);
+      }, (err) => {
+        this.isLoading = false;
+        console.log('FAILED...', err);
+      });
+
+    contactForm.reset();
+  }
+
+  hideAlertAfterSeconds(milliseconds: number) {
+    setTimeout(() => {
+      this.senderName = '';
+    }, milliseconds);
+  }
 }
